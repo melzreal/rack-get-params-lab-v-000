58702a83feb7e94007b19f72f7778009843ed49d
Melissa Real <melissa.real@hotmail.com>
2018-10-25 14:52:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -33,5 +34,5 @@ class Application
   end
 
 
-  
+
 end
