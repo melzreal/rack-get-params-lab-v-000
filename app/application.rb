4fea021251e11f55eca5b39acd9ff6c9b50f67b9
Melissa Real <melissa.real@hotmail.com>
2018-10-25 17:23:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,16 +21,6 @@ class Application
         else    @@cart.each do |item|
             resp.write "#{item}\n"
         end
-    elsif req.path.match(/search/)
-
-   search_term = req.params["q"]
-
-   if @@cart.include?(search_term)
-     resp.write "#{search_term} is one of our items"
-   else
-     resp.write "Couldn't find #{search_term}"
-   end
-
   else
       resp.write "Path Not Found"
     end
